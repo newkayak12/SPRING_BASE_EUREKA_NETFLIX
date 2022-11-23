963fd4d3c7b538d0cd5cce7259a1469d52eb051c
newkayak12 <newkayak12@gmail.com>
2022-11-23 22:25:26 +0900

    init

---
+++ src/main/java/com/base/spring_base_eureka/SpringBaseEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.base.spring_base_eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBaseEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBaseEurekaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/base/spring_base_eureka/SpringBaseEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.base.spring_base_eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBaseEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
